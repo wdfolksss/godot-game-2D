664635a434f64b40d7c686ae7420cb7fea3e5b42
wdfolksss <amaliawarda411@gmail.com>
2026-08-20 12:34:04 +0700

    game 2D done

---
+++ main.gd
@@ -0,0 +1,57 @@
+extends Node
+@export var mob_scene = preload("res://mob.tscn")
+var score 
+
+func  _ready() -> void:
+	#new_game()	
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func game_over() -> void:
+	$HUD.show_game_over()
+	$ScoreTimer.stop()
+	$DeathSound.play()
+	$Music.stop()
+	
+func new_game():
+	score = 0
+	$player.get_node("CollisionShape2D").set_deferred("disabled", false)
+	$player.position = $StartPosition.position
+	$player.show()
+	$StartTimer.start()	
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
+
+func _on_start_timer_timeout() -> void:
+	$MobTimer.start()
+	$ScoreTimer.start()
+
+
+func _on_score_timer_timeout() -> void:
+	score += 1
+	$HUD.update_score(score)	
+
+
+func _on_mob_timer_timeout() -> void:
+	var mob = mob_scene.instantiate()
+	var mob_spawn_location = $MobPath/MobSpawnLocation
+	mob_spawn_location.progress_ratio = randf()
+
+	mob.position = mob_spawn_location.position
+	
+	var direction  = mob_spawn_location.rotation + PI / 2
+	
+	direction += randf_range(-PI / 4, PI / 4)
+	mob.rotation = direction 
+	
+	var velocity  = Vector2(randf_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+	 
+	add_child(mob)
+	
